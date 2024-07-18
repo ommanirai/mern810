64416c29aed4a25b9c9c5c783435189356055987
ommanirai <ommanirai27@gmail.com>
2024-07-18 09:15:03 +0545

    for loop:

---
+++ js/loop.js
@@ -0,0 +1,80 @@
+// console.log("run js file using node js")
+/*
+node js
+check node js existence or version
+command:
+node --version
+node -v
+*/
+
+/*
+loop
+types:
+1. for loop
+syntax:
+for(initialization;condition;increment/decrement){
+    body of for
+}
+1. wap(write a program) to print a number from 1 to 10
+=> initialization:
+        i = 1
+=>  condition:
+i<=10
+or
+i<11
+=>  increment
+i++
+
+for(i=1;i<=10;i++){
+    console.log(i)
+}
+
+2. wap to print a number from 10 to 1
+initialization: i = 10
+condition: i>=1 or i > 0
+decrement: i--
+
+for(i=10;i>0;i--){
+    console.log(i)
+}
+
+3. wap to print a even number from 1 to 50
+i=1
+i<=50
+i++
+i = 1 to 50
+if(i%2==0){
+}
+
+for (i = 1; i <= 50; i++) {
+    // i = 1 to 50
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+for (i = 2; i <= 50; i=i+2) {
+    console.log(i)
+}
+
+4. wap to print the multiplication of a number
+n = 4
+i = 1 to 10
+n*i
+
+var n = 4
+for(i=1;i<=10;i++){
+    // console.log(i*n)
+    console.log(n+" x "+i+" = "+n*i)
+}
+
+5. wap to print the sum of number from 1 to 10
+sum = 55
+
+6. wap to print the factorial of a number 5
+
+2. while loop
+3. do while loop
+
+dom(prompt)
+*/
