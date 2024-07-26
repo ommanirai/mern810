73983302890c70d30aaff5b5479ba44013f01812
ommanirai <ommanirai27@gmail.com>
2024-07-26 09:20:00 +0545

    methods

---
+++ js/arraymethods.js
@@ -0,0 +1,36 @@
+// array loop
+/*
+1. map
+2. fileter
+3. reduce
+4. every
+5. some
+6. find
+7. forEach
+*/
+var fruits = ['kiwi', 'orange', 'strawberry']
+// add element in array
+// at last position
+// fruits.push("banana")
+
+// at first posiiton
+// fruits.unshift("apple")
+
+// from last position
+// fruits.pop()
+// from first position
+// fruits.shift()
+
+// slice
+// console.log(fruits.slice(0, 2))
+
+// splice
+// add in any position
+// remove from any position
+fruits.splice(1,2)
+
+// start index
+// number of item to remove
+// item name(add)
+
+console.log(fruits)
---
+++ js/method.js
@@ -0,0 +1,53 @@
+/*
+method
+=> js inbuilt function
+
+setTimeout()
+setInterval()
+clearInterval()
+prompt()
+Number()
+typeof()
+
+properties
+
+*/
+// string
+var text = "Good Morning"
+var fruits = 'apple, kiwi, papaya'
+// console.log(text.length)
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// // string to array
+// console.log(fruits.split(','))
+
+// number
+var n = 100.246735
+// console.log(n.toFixed(2))
+// console.log(n.toFixed(0))
+
+// boolen
+// undefined
+// null
+
+// object
+var details = {
+    name:"Sujan",
+    address:"Vedu",
+    phone:'9823424234'
+}
+// console.log(Object.keys(details))
+// console.log(Object.values(details))
+// console.log(details.hasOwnProperty('names'))
+
+// for, while, do while
+// object loop
+// for in
+for(var item in details){
+    // console.log(item)
+    // console.log(details[item])
+    // console.log(details.item)
+    console.log("value under key ",item, ':', details[item])
+}
+
+
