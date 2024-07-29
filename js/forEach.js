ab33afc90abe98e1af8a5753661135d6aeffb749
ommanirai <ommanirai27@gmail.com>
2024-07-29 09:59:07 +0545

    unique array

--- js/forEach.js
+++ js/forEach.js
@@ -14,34 +14,34 @@ var fruits = ['papaya', 'kiwi', 'apple', 'orange', 'kiwi', 'papaya']
 
 // data => information => knowledge
 
-var uniqueArray = []
-fruits.forEach(function (item, index) {
-    // if(uniqueArray.indexOf(item)==-1){
-    //     uniqueArray.push(item)    
-    // }
+// var uniqueArray = []
+// fruits.forEach(function (item, index) {
+// if(uniqueArray.indexOf(item)==-1){
+//     uniqueArray.push(item)    
+// }
 
-    // if(uniqueArray.includes(item)){
+// if(uniqueArray.includes(item)){
 
-    // }
-    // else{
-    //     uniqueArray.push(item)    
+// }
+// else{
+//     uniqueArray.push(item)    
 
-    // }
+// }
 
-    if (!uniqueArray.includes(item)) {
-        uniqueArray.push(item)
-    }
-})
-console.log(uniqueArray)
+//     if (!uniqueArray.includes(item)) {
+//         uniqueArray.push(item)
+//     }
+// })
+// console.log(uniqueArray)
 
 
-var uniqueNumber = []
-var number = [1,2,3,4,5,23,4,3,4]
-number.forEach(function(item, index){
-    if(!uniqueNumber.includes(item)){
-        uniqueArray.push(item)
-    }
-})
+// var uniqueNumber = []
+var number = [1, 2, 3, 4, 5, 23, 4, 3, 4]
+// number.forEach(function(item, index){
+//     if(!uniqueNumber.includes(item)){
+//         uniqueArray.push(item)
+//     }
+// })
 
 // uniqueItem(array_name)
 // uniqueItem(number)
@@ -51,11 +51,30 @@ number.forEach(function(item, index){
 // 2. function with argument
 
 
-var fruits = ['papaya', 'kiwi', 'apple', 'orange', 'kiwi', 'papaya']
+var fruit = ['papaya', 'kiwi', 'apple', 'orange', 'kiwi', 'papaya']
 // result
-var result = {
-    "papaya":2,
-    'kiwi':2,
-    "apple":1,
-    "orange":1
+// var result = {
+//     "papaya":2,
+//     'kiwi':2,
+//     "apple":1,
+//     "orange":1
+// }
+var bikes = ['honda', 'yamaha', 'honda','yamaha']
+
+function uniqueItem(args) {
+    var uniqueArray = []
+    args.forEach(function (item, index) {
+        if(!uniqueArray.includes(item)){
+            uniqueArray.push(item)
+        }
+    })
+    // console.log(uniqueArray)
+    return uniqueArray
 }
+
+var f = uniqueItem(fruit)
+console.log(f)
+var b = uniqueItem(number)
+console.log(b)
+var a = uniqueItem(bikes)
+console.log(a)
